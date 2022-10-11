e4b52958eda11db71c9a6e0079d643be0f8ff188
Sudharshan143 <112839681+Sudharshan143@users.noreply.github.com>
2022-10-11 16:35:06 +0530

    Create day6Factorial of n(17).py

---
+++ day6Factorial of n(17).py
@@ -0,0 +1,12 @@
+try:
+    def factorial(n):
+        return 1 if (n==1 or n==0) else n * factorial(n - 1);
+    num = int(input("enter the number: "));
+
+    if (num>0):
+        print("Factorial of",num,"is",
+        factorial(num))
+    else:
+        print("invalid")
+except ValueError:
+    print("invalid")
